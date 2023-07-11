e61bdaac33ca3ca7a2d6c12536241026787985df
Sun, Ceping <cepingx.sun@intel.com>
2023-07-11 17:17:52 +0530

    Add -I../openssl-stubs in CFLAGS to build openssl
    Add the -I../openssl-stubs in CFLAGS to fix the build error
    CrtLibSupport.h is not found when openssl3.1.1 build the object in providers
    
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>

--- deps/rust-tpm-20-ref/sh_script/build.sh
+++ deps/rust-tpm-20-ref/sh_script/build.sh
@@ -34,7 +34,7 @@ function build() {
     CC=clang AR=llvm-ar \
         CFLAGS="-Wall -Werror -Wno-format -target x86_64-unknown-none -fPIC \
         -nostdlib -nostdlibinc -ffreestanding -Istd-include -Iconf-include \
-        -Iarch/x86_64 -include CrtLibSupport.h -std=c99" \
+        -Iarch/x86_64 -I../openssl-stubs -include CrtLibSupport.h -std=c99" \
         ./process_openssl.pl
 
     make -j$(nproc) libcrypto.a
