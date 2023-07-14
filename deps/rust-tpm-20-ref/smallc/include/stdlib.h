4f65c9a0b566567714f76e2041edcb5f51a44f01
Min M Xu <min.m.xu@intel.com>
2023-07-13 21:09:49 -0400

    Add memory function declarations in smallc
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- deps/rust-tpm-20-ref/smallc/include/stdlib.h
+++ deps/rust-tpm-20-ref/smallc/include/stdlib.h
@@ -7,5 +7,7 @@ int atoi(const char *);
 
 unsigned long strtoul(const char *, char **, int);
 long strtol(const char *, char **, int);
-
+void *malloc(size_t n);
+void *realloc(void *p, size_t n);
+void free(void *p);
 #endif
