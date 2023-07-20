f590f780eb050910ea4bc162180371c183f50f11
Sun, Ceping <cepingx.sun@intel.com>
2023-07-20 17:32:14 +0530

    Add unit test in src/global
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>

--- src/global/src/spdm.rs
+++ src/global/src/spdm.rs
@@ -111,3 +111,52 @@ impl GlobalSpdmData {
         }
     }
 }
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    const DATA_SIZE: usize = 0x100;
+    const INVALID_DATA_SIZE: usize = 0x2000;
+    const INVALIDPKCS8_DOCUMENT_MAX_LEN: usize = 0x2000;
+
+    #[test]
+    fn test_globalspdmdata() {
+        let mut globalspdmdata = GlobalSpdmData::default();
+        let vtpmid = 1 as u128;
+        globalspdmdata.set_vtpm_id(vtpmid);
+        assert_eq!(globalspdmdata.vtpm_id().is_err(), true);
+        assert_eq!(globalspdmdata.operation().is_err(), true);
+        assert_eq!(globalspdmdata.data().is_none(), true);
+        assert_eq!(globalspdmdata.pkcs8().is_none(), true);
+        assert_eq!(vtpmid, globalspdmdata.vtpm_id);
+        globalspdmdata.valid = true;
+        assert_eq!(vtpmid, globalspdmdata.vtpm_id().unwrap());
+        let operation = TdVtpmOperation::Create;
+        globalspdmdata.set_operation(operation);
+        assert_eq!(operation, globalspdmdata.operation().unwrap());
+        let mut data1 = [0xffu8; INVALID_DATA_SIZE];
+        let result = globalspdmdata.set_data(&mut data1);
+        assert_eq!(result.is_err(), true);
+        let mut data2 = [0xffu8; DATA_SIZE];
+        let result = globalspdmdata.set_data(&mut data2);
+        assert_eq!(result.unwrap(), DATA_SIZE);
+        assert_eq!(globalspdmdata.data().unwrap(), data2);
+        globalspdmdata.clear_data();
+        assert_eq!(globalspdmdata.data_size, 0);
+        let mut pkcs8test1 = [0x89u8; INVALIDPKCS8_DOCUMENT_MAX_LEN];
+        let result1 = globalspdmdata.set_pkcs8(&mut pkcs8test1);
+        assert_eq!(result1.is_err(), true);
+        let mut pkcs8test2: [u8; PKCS8_DOCUMENT_MAX_LEN] = [0x89; PKCS8_DOCUMENT_MAX_LEN];
+        let result1 = globalspdmdata.set_pkcs8(&mut pkcs8test2);
+        assert_eq!(result1.unwrap(), PKCS8_DOCUMENT_MAX_LEN);
+        assert_eq!(globalspdmdata.pcks8_size, PKCS8_DOCUMENT_MAX_LEN);
+        assert_eq!(globalspdmdata.pkcs8().unwrap(), pkcs8test2);
+        globalspdmdata.clean_pkcs8();
+        let zeropkc8: [u8; PKCS8_DOCUMENT_MAX_LEN] = [0; PKCS8_DOCUMENT_MAX_LEN];
+        assert_eq!(globalspdmdata.pkcs8().unwrap(), zeropkc8);
+        globalspdmdata.clear();
+        assert_eq!(globalspdmdata.vtpm_id().is_err(), true);
+    }
+}
--- src/global/src/tpm.rs
+++ src/global/src/tpm.rs
@@ -54,3 +54,36 @@ impl GlobalTpmData {
         sensitive_data_cleanup(&mut self.nv_mem);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INVALID_NV_SIZE: usize = 0x2000;
+
+    #[test]
+    fn test_function() {
+        let mut globaltpmdata = GlobalTpmData::default();
+        assert_eq!(globaltpmdata.tpm_active, globaltpmdata.tpm_active());
+        let active = true;
+        globaltpmdata.set_tpm_active(active);
+        assert_eq!(globaltpmdata.tpm_active, globaltpmdata.tpm_active());
+        assert_eq!(globaltpmdata.tpm_active, active);
+        assert_eq!(globaltpmdata.tpm_active(), active);
+        assert_eq!(globaltpmdata.nv_mem, globaltpmdata.tpm2_nv_mem());
+        let invalid_nv = [1; INVALID_NV_SIZE];
+        let res = globaltpmdata.set_nv_mem(&invalid_nv);
+        assert!(res.is_err());
+
+        let nv_mem_data: [u8; TPM2_NV_SIZE] = [1; TPM2_NV_SIZE];
+        let res = globaltpmdata.set_nv_mem(&nv_mem_data);
+        assert_eq!(res.unwrap(), TPM2_NV_SIZE);
+        assert_eq!(globaltpmdata.nv_mem, nv_mem_data);
+        assert_eq!(globaltpmdata.tpm2_nv_mem(), nv_mem_data);
+        globaltpmdata.clean_nv_mem();
+        let zerodata: [u8; TPM2_NV_SIZE] = [0; TPM2_NV_SIZE];
+        assert_eq!(globaltpmdata.nv_mem, globaltpmdata.tpm2_nv_mem());
+        assert_eq!(globaltpmdata.nv_mem, zerodata);
+        assert_eq!(globaltpmdata.tpm2_nv_mem(), zerodata);
+    }
+}
